123c92f02dd18d8dc693aaa173883e5a48ebe97b
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2023-07-12 11:54:51 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+# first we import sinatra so we have access to its 
+# objects and methods
+require 'sinatra'
+
+# require active record to make use of it
+require 'sinatra/activerecord'
+
+get('/') do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end

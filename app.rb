2498c506887bd26ab87604e8260f528aca05f539
Ben Purinton <ben.purinton@gmail.com>
2023-07-17 15:18:39 -0800

    cleanup, fix quotes

--- app.rb
+++ app.rb
@@ -7,41 +7,41 @@ end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
-	second_die = rand(1..6)
+  second_die = rand(1..6)
   sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	erb(:two_six)
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
-	second_die = rand(1..10)
+  second_die = rand(1..10)
   sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
   @die = rand(1..20)
-	
-	@outcome = "You rolled a #{@die}."
-	
-	erb(:one_twenty)
+
+  @outcome = "You rolled a #{@die}."
+
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
   first_die = rand(1..4)
-	second_die = rand(1..4)
+  second_die = rand(1..4)
   third_die = rand(1..4)
   fourth_die = rand(1..4)
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
-	@outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
-	
+
+  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+
   erb(:five_four)
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,29 +1,12 @@
-require './app'
-
-configure do
-  # setup a database connection
-  set(:database, {adapter: "sqlite3", database: "db/development.sqlite3"})
-end
+require "./app"
 
 configure :development do
-  require "sinatra/reloader"
-
   # we would also like a nicer error page in development
-  require 'better_errors'
-  require 'binding_of_caller'
-  
+  require "better_errors"
+  require "binding_of_caller"
+
   # need this configure for better errors
   use(BetterErrors::Middleware)
   BetterErrors.application_root = __dir__
-  BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
-
-  # appdev support patches
-  require "appdev_support"
-
-  AppdevSupport.config do |config|
-    # config.action_dispatch = true;
-    # config.active_record = true;
-    config.pryrc = :full;
-    end
-  AppdevSupport.init
+  BetterErrors::Middleware.allow_ip!("0.0.0.0/0.0.0.0")
 end

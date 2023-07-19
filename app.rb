692f6c4cc7630b1195bb16e35b54d4d1508ed880
Ben Purinton <ben.purinton@gmail.com>
2023-07-18 17:24:41 -0800

    Update starting point (#2)

--- app.rb
+++ app.rb
@@ -6,42 +6,49 @@ get("/") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
 
   erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
 
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  @die = rand(1..20)
+  @rolls = []
 
-  @outcome = "You rolled a #{@die}."
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
 
   erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
 
   erb(:five_four)
 end

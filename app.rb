fc562a11758142701e8680d6b85e736e8ea3a532
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-15 18:59:15 +0000

    final_version

--- app.rb
+++ app.rb
@@ -5,50 +5,20 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
+
+
+get("/dice/:number_of_dice/:how_many_sides") do
+  @num_dice = params.fetch("number_of_dice").to_i
+  @num_side = params.fetch("how_many_sides").to_i
+  
+
   @rolls = []
 
-  2.times do
-    die = rand(1..6)
+  @num_dice.times do
+    die = rand(1..@num_side)
 
     @rolls.push(die)
   end
 
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
+  erb(:flexible)
 end
